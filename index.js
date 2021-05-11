bb4409d7eed28104b346898b384e040f223fa130
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-05-11 04:10:10 +0300

    refactor

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log("hey");
-
-debugger
-
-
-console.log("bye")
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,18 +6,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/facebook-api', {
     useUnifiedTopology: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
 
-const me = new User({
-    name: 'Atalia',
-    age: 30
-})
 
-me.save()
+// const me = new User({
+//     name: '      Meli',
+//     email: "ATALIA@NANA.CO.IL    ",
+//     password: "Passwordert5    "
+// })
+
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((e) => { console.log(e) })
