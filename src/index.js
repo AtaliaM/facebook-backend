1c02a96c6999815b217326676336f131b71794e0
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-05-16 04:44:54 +0300

    more routes

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ app.use(express.json());
 app.use(cors());
 
 
-app.post('/users', async (req,res)=> {
+app.post('/users', async (req, res) => {
     console.log(req.body);
     const user = new User(req.body);
     try {
         await user.save();
         res.status(201).send(user);
     }
-    catch(e) {
+    catch (e) {
         res.status(400).send(e);
     }
 
@@ -32,12 +32,12 @@ app.post('/users', async (req,res)=> {
     // });
 })
 
-app.get('/users', async (req,res)=> {
+app.get('/users', async (req, res) => {
 
     try {
         const users = await User.find({});
         res.send(users);
-    } catch(e) {
+    } catch (e) {
         res.status(500).send();
     }
     // User.find({}).then((users)=> {
@@ -47,7 +47,7 @@ app.get('/users', async (req,res)=> {
     // });
 })
 
-app.get('/users/:id', async (req,res)=> {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
 
     try {
@@ -56,7 +56,7 @@ app.get('/users/:id', async (req,res)=> {
             return res.status(404).send();
         }
         res.send(user);
-    } catch(e) {
+    } catch (e) {
         res.status(500).send();
     }
 
@@ -70,20 +70,29 @@ app.get('/users/:id', async (req,res)=> {
     // })
 })
 
-app.patch('/users/:id', async(res,req)=> {
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'password', 'email', 'age'];
+    const isValidOperation = updates.every((update)=> {
+        return allowedUpdates.includes(update);
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: "Invalid Updates"});
+    }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true});
-        if(!user) {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!user) {
             return res.status(404).send();
         }
         res.send(user);
-    } catch(e) {
+    } catch (e) {
         res.status(400).send(e);
     }
 
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`server is up on port ${port}`);
 })
