49fc2bbad89ed6e522889a558d4b795061ac1792
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-05-16 04:22:05 +0300

    more routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require("cors");
 require("./db/mongoose");
 const User = require("./models/user");
+const { findByIdAndUpdate } = require('./models/user');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,24 +14,76 @@ app.use(express.json());
 app.use(cors());
 
 
-app.post('/users', (req,res)=> {
+app.post('/users', async (req,res)=> {
     console.log(req.body);
     const user = new User(req.body);
-    user.save().then(()=> {
-        res.status(201).send(user)}
-    ).catch((e)=>{
+    try {
+        await user.save();
+        res.status(201).send(user);
+    }
+    catch(e) {
         res.status(400).send(e);
-    });
+    }
+
+    // user.save().then(()=> {
+    //     res.status(201).send(user)}
+    // ).catch((e)=>{
+    //     res.status(400).send(e);
+    // });
 })
 
-app.get('/users', (req,res)=> {
-    User.find({}).then((users)=> {
+app.get('/users', async (req,res)=> {
+
+    try {
+        const users = await User.find({});
         res.send(users);
-    }).catch((e)=> {
-        res.status(500).send()
-    });
+    } catch(e) {
+        res.status(500).send();
+    }
+    // User.find({}).then((users)=> {
+    //     res.send(users);
+    // }).catch((e)=> {
+    //     res.status(500).send()
+    // });
 })
 
-app.listen((port, ()=> {
-    console.log("server is up on port " + port);
-}))
+app.get('/users/:id', async (req,res)=> {
+    const _id = req.params.id;
+
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch(e) {
+        res.status(500).send();
+    }
+
+    // User.findById(_id).then((user)=> {
+    //     if (!user) {
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch((e)=> {
+    //     res.status(500).send();
+    // })
+})
+
+app.patch('/users/:id', async(res,req)=> {
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true});
+        if(!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch(e) {
+        res.status(400).send(e);
+    }
+
+})
+
+app.listen(port, ()=> {
+    console.log(`server is up on port ${port}`);
+})
