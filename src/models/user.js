c6a0da165cf263f3d793c1b82fbd405e5caa1095
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-05-18 12:19:07 +0300

    added jwt token to user model and routes

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,11 +40,27 @@ const userSchema = new mongoose.Schema({
                 throw new Error("password cannot contain 'password'")
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            require: true
+        }
+    }]
 
 })
 
-userSchema.statics.findByCredentials = async (email,password) => {
+userSchema.methods.generateAuthToken = async function () { // instance method
+    const user = this;
+    const token = jwt.sign({_id : user._id.toString()}, 'thisismyawesomefacebookapp');
+
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+
+    return token;
+}
+
+userSchema.statics.findByCredentials = async (email,password) => { //model method
     const user = await User.findOne({email});
 
     if(!user) {
