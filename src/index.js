8db9885b87486341ba71223feeda59be40e06a0a
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-05-18 10:06:02 +0300

    routers files for user and post

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require("cors");
 require("./db/mongoose");
-const User = require("./models/user");
-const { findByIdAndUpdate } = require('./models/user');
-
+const userRouter = require("./routers/user");
+// const postRouter = require("./routers/post");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,86 +12,25 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-
-app.post('/users', async (req, res) => {
-    console.log(req.body);
-    const user = new User(req.body);
-    try {
-        await user.save();
-        res.status(201).send(user);
-    }
-    catch (e) {
-        res.status(400).send(e);
-    }
-
-    // user.save().then(()=> {
-    //     res.status(201).send(user)}
-    // ).catch((e)=>{
-    //     res.status(400).send(e);
-    // });
-})
-
-app.get('/users', async (req, res) => {
-
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send();
-    }
-    // User.find({}).then((users)=> {
-    //     res.send(users);
-    // }).catch((e)=> {
-    //     res.status(500).send()
-    // });
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (e) {
-        res.status(500).send();
-    }
-
-    // User.findById(_id).then((user)=> {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e)=> {
-    //     res.status(500).send();
-    // })
-})
-
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['name', 'password', 'email', 'age'];
-    const isValidOperation = updates.every((update)=> {
-        return allowedUpdates.includes(update);
-    })
-
-    if (!isValidOperation) {
-        return res.status(400).send({error: "Invalid Updates"});
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-
-})
+app.use(userRouter);
+// app.use(postRouter);
 
 app.listen(port, () => {
     console.log(`server is up on port ${port}`);
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = "Red12345!";
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword);
+    console.log(isMatch)
+
+}
+
+myFunction();
---
+++ src/routers/post.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const router = new express.Router();
+const Post = require("../models/post");
+const auth = require("../middleware/auth");
+const { findOneAndDelete } = require('../models/post');
+
+
+router.post("/posts", auth, async (req, res) => {
+    // const post = new post(req.body);
+    const post = new Post({
+        ...req.body, //copiyng all the properties from body
+        owner: req.user._id
+    })
+
+    try {
+        await post.save();
+        res.status(201).send(post);
+    }
+    catch (e) {
+        res.status(400).send(e);
+    }
+
+    console.log(post);
+})
+
+router.patch("/posts/:id", auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["description", "complete"];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    })
+
+    if (!isValidOperation) {
+        res.status(400).send(({ error: "Invalid update" }));
+    }
+
+    try {
+        // const post = await post.findById(req.params.id);
+        const post = await Post.findOne({ _id: req.params.id, owner: req.user._id })
+
+        if (!post) {
+            return res.status(404).send();
+        }
+        
+        updates.forEach((update) => {
+            post[update] = req.body[update];
+        })
+        await post.save();
+        // const post = await post.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true});
+
+        res.send(post);
+    }
+    catch (e) {
+        res.status(400).send(e);
+    }
+})
+
+
+router.get("/posts", auth, async (req, res) => {
+
+    try {
+        const posts = await Post.find({ owner: req.user._id });
+        res.send(posts);
+    }
+    catch (e) {
+        res.status(500).send();
+    }
+
+})
+
+router.get("/posts/:id", auth, async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        // const post = await post.findById(_id);
+        const post = await Post.findOne({ _id, owner: req.user._id })
+        if (!post) {
+            return res.status(404).send();
+        }
+        res.send(post);
+    }
+    catch (e) {
+        res.status(500).send();
+    }
+
+})
+
+router.delete("/posts/:id",auth, async (req, res) => {
+    try {
+        // const post = await post.findByIdAndDelete(req.params.id);
+        const post = await findOneAndDelete({_id: req.params.id, owner: req.user._id})
+        if (!post) {
+            return res.status(404).send();
+        }
+        res.send(post);
+    }
+    catch (e) {
+        res.status(500).send(e);
+    }
+})
+
+module.exports = router;
