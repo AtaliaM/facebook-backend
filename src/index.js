b94b5c98587fda832e4c9c58ce51b579971cee2f
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-05-29 07:18:34 +0300

    added NODE_ENV on .env, on package.json dev script and on index.js

--- src/index.js
+++ src/index.js
@@ -11,17 +11,6 @@ console.log(environment);
 console.log(process.env.NODE_ENV);
 const host = '0.0.0.0';
 
-if (process.env.NODE_ENV === 'prod') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-
 //configuring express to automatically parse the incoming json for us,
 //so we have it as an object we can use
 app.use(express.json());
