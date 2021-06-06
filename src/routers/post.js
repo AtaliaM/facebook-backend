fc23b4d84b1a4824f3f2cdc905e840127995fae5
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-06-06 17:17:23 +0300

    refactoring

--- src/routers/post.js
+++ src/routers/post.js
@@ -2,104 +2,115 @@ const express = require('express');
 const router = new express.Router();
 const Post = require("../models/post");
 const auth = require("../middleware/auth");
-const { findOneAndDelete } = require('../models/post');
 
-//add post//
-router.post("/posts", auth, async (req, res) => {
-    // const post = new post(req.body);
-    const post = new Post({
-        ...req.body, //copiyng all the properties from body
-        owner: req.user._id
-    })
+defineAddNewPostEndpoint();
+defineEditPostEndpoint();
+defineGetAllPostsByUserEndpoint();
+defineGetSpecificPostByIdEndpoint();
+defineDeletePostByIdEndpoint();
 
-    try {
-        await post.save();
-        res.status(201).send(post);
-    }
-    catch (e) {
-        res.status(400).send(e);
-    }
-
-    console.log(post);
-})
-
-//edit post//
-router.patch("/posts/:id", auth, async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ["postBody", "postHeader"];
-    const isValidOperation = updates.every((update) => {
-        return allowedUpdates.includes(update);
-    })
-
-    if (!isValidOperation) {
-        res.status(400).send(({ error: "Invalid update" }));
-    }
-
-    try {
-        // const post = await post.findById(req.params.id);
-        const post = await Post.findOne({ _id: req.params.id, owner: req.user._id })
-
-        if (!post) {
-            return res.status(404).send();
-        }
-        
-        updates.forEach((update) => {
-            post[update] = req.body[update];
+function defineAddNewPostEndpoint() {
+    return (
+        router.post("/posts", auth, async (req, res) => {
+            const post = new Post({
+                ...req.body, 
+                owner: req.user._id
+            })
+            try {
+                await post.save();
+                res.status(201).send(post);
+            }
+            catch (e) {
+                res.status(400).send(e);
+            }
         })
-        await post.save();
-        // const post = await post.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true});
+    )
+}
 
-        res.send(post);
-    }
-    catch (e) {
-        res.status(400).send(e);
-    }
-})
+function defineEditPostEndpoint() {
+    return (
+        router.patch("/posts/:id", auth, async (req, res) => {
+            const updates = Object.keys(req.body);
+            const allowedUpdates = ["postBody", "postHeader"];
+            const isValidOperation = updates.every((update) => {
+                return allowedUpdates.includes(update);
+            })
+        
+            if (!isValidOperation) {
+                res.status(400).send(({ error: "Invalid update" }));
+            }
+        
+            try {
+                // const post = await post.findById(req.params.id);
+                const post = await Post.findOne({ _id: req.params.id, owner: req.user._id })
+                if (!post) {
+                    return res.status(404).send();
+                }
+                
+                updates.forEach((update) => {
+                    post[update] = req.body[update];
+                })
+                await post.save();
+                res.send(post);
+            }
+            catch (e) {
+                res.status(400).send(e);
+            }
+        })
+    )
+}
 
-//get all posts of a specific user//
-router.get("/posts", auth, async (req, res) => {
+function defineGetAllPostsByUserEndpoint() {
+    return (
+        router.get("/posts", auth, async (req, res) => {
+        
+            try {
+                const posts = await Post.find({ owner: req.user._id });
+                res.send(posts);
+            }
+            catch (e) {
+                res.status(500).send();
+            }
+        
+        })
+    )
+}
 
-    try {
-        const posts = await Post.find({ owner: req.user._id });
-        res.send(posts);
-    }
-    catch (e) {
-        res.status(500).send();
-    }
+function defineGetSpecificPostByIdEndpoint() {
+    return (
+        router.get("/posts/:id", auth, async (req, res) => {
+            const _id = req.params.id;
+        
+            try {
+                const post = await Post.findOne({ _id, owner: req.user._id })
+                if (!post) {
+                    return res.status(404).send();
+                }
+                res.send(post);
+            }
+            catch (e) {
+                res.status(500).send();
+            }
+        
+        })
+    )
+}
 
-})
-
-//get a specific user post by id//
-router.get("/posts/:id", auth, async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        // const post = await post.findById(_id);
-        const post = await Post.findOne({ _id, owner: req.user._id })
-        if (!post) {
-            return res.status(404).send();
-        }
-        res.send(post);
-    }
-    catch (e) {
-        res.status(500).send();
-    }
-
-})
-
-//delete a specific post by id//
-router.delete("/posts/:id",auth, async (req, res) => {
-    try {
-        // const post = await post.findByIdAndDelete(req.params.id);
-        const post = await findOneAndDelete({_id: req.params.id, owner: req.user._id})
-        if (!post) {
-            return res.status(404).send();
-        }
-        res.send(post);
-    }
-    catch (e) {
-        res.status(500).send(e);
-    }
-})
+function defineDeletePostByIdEndpoint() {
+    return (
+        router.delete("/posts/:id",auth, async (req, res) => {
+            try {
+                const post = await Post.findOneAndDelete({_id: req.params.id, owner: req.user._id})
+                if (!post) {
+                    return res.status(404).send();
+                }
+                res.send(post);
+            }
+            catch (e) {
+                res.status(500).send(e);
+            }
+        })
+    )
+}
 
 module.exports = router;
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,175 +3,210 @@ const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
 
-//register/add new user//
-router.post('/users', async (req, res) => {
-    console.log(req.body);
-    //creating user's path//
-    const path = `${req.body.firstName}-${new Date().getTime()}`;
-    const userObj = { ...req.body, path };
+defineRegisterUserEndpoint();
+defineLoginUserEndpoint();
+definelogoutUserEndpoint();
+defineLogoutAllDevicesUserEndpoint();
+defineGetAllUsersEndpoint();
+defineGetUserProfileEndpoint();
+defineGetUserByPathEndpoint();
+defineAddToMyFollowingEndpoint();
+defineRemoveUserFromMyFollowingEndpoint();
+defineUpdateMyProfileEndpoint();
+defineDeleteMyProfileEndpoint();
 
-    const user = new User(userObj);
-    try {
-        await user.save();
-        const token = await user.generateAuthToken();
-        res.status(201).send({ user, token });
-    }
-    catch (e) {
-        res.status(400).send(e);
-    }
-})
-
-//login user//
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password);
-        const token = await user.generateAuthToken();
-        res.send({ user, token });
-    } catch (e) {
-        res.status(400).send();
-    }
-})
-
-//logout user//
-router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token;
+function defineRegisterUserEndpoint() {
+    return (
+        router.post('/users', async (req, res) => {
+            console.log(req.body);
+            //creating user's path//
+            const path = `${req.body.firstName}-${new Date().getTime()}`;
+            const userObj = { ...req.body, path };
+        
+            const user = new User(userObj);
+            try {
+                await user.save();
+                const token = await user.generateAuthToken();
+                res.status(201).send({ user, token });
+            }
+            catch (e) {
+                res.status(400).send(e);
+            }
         })
-
-        await req.user.save();
-        res.send();
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-//logout user fron all devices//
-router.post('/users/logoutAll', auth, async (req, res) => {
-    try {
-        req.user.tokens = [];
-
-        await req.user.save();
-        res.send();
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-//fetching all users-for dev purposes//
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send();
-    }
-
-})
-
-//my profile//
-router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user);
-})
-
-//get user by path//
-router.get('/users/:path', async (req, res) => {
-    const path = req.params.path;
-    try {
-        const user = await User.find({ path });
-        // console.log(user)
-        // const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (e) {
-        res.status(500).send();
-    }
-
-})
+    )
+}
 
 
-//add user to my following list by user path//
-router.patch('/users/me/followUser', auth, async (req, res) => {
-    const path = req.body.path;
-    try {
-        // const userToFollow = await User.find({ path });
-        const userToFollow = await User.findOneAndUpdate({ path }, 
-            // {'user._id': {$ne: ObjectId(req.user._id)}},
-            {$addToSet: {myFollowers: {userId: req.user._id}}});
-        if (!userToFollow) {
-            return res.status(404).send();
-        }
-        await req.user.updateOne(
-            // {'user._id': {$ne: userToFollow._id}},
-            { $addToSet: { usersIFollow: { userId: userToFollow._id } } }
-        );
-
-        res.send(req.user);
-
-    } catch (e) {
-        res.status(400).send(e);
-    }
-})
-
-
-//remove user from my following list//
-router.patch('/users/me/unfollowUser', auth, async (req, res) => {
-    const path = req.body.path;
-    try {
-        const userToUnfollow = await User.findOneAndUpdate({ path }, 
-            { $pull: { myFollowers: { userId: req.user._id } } });
-        if (!userToUnfollow) {
-            return res.status(404).send();
-        }
-        await req.user.updateOne(
-            { $pull: { usersIFollow: { userId: userToUnfollow._id } } }
-        );
-
-        res.send(req.user);
-
-    } catch (e) {
-        res.status(400).send(e);
-    }
-})
-
-
-//update my profile//
-router.patch('/users/me', auth, async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['firstName', 'lastName', 'password', 'email', 'age'];
-    const isValidOperation = updates.every((update) => {
-        return allowedUpdates.includes(update);
-    })
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: "Invalid Updates" });
-    }
-
-    try {
-        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        updates.forEach((update) => {
-            req.user[update] = req.body[update];
+function defineLoginUserEndpoint() {
+    return (
+        router.post('/users/login', async (req, res) => {
+            try {
+                const user = await User.findByCredentials(req.body.email, req.body.password);
+                const token = await user.generateAuthToken();
+                res.send({ user, token });
+            } catch (e) {
+                res.status(400).send();
+            }
         })
-        await req.user.save();
+    )
+}
 
-        res.send(req.user);
-    } catch (e) {
-        res.status(400).send(e);
-    }
+function definelogoutUserEndpoint() {
+    return (
+        router.post('/users/logout', auth, async (req, res) => {
+            try {
+                req.user.tokens = req.user.tokens.filter((token) => {
+                    return token.token !== req.token;
+                })
+        
+                await req.user.save();
+                res.send();
+            } catch (e) {
+                res.status(500).send()
+            }
+        })
+    )
+}
 
-})
+function defineLogoutAllDevicesUserEndpoint() {
+    return (
+        router.post('/users/logoutAll', auth, async (req, res) => {
+            try {
+                req.user.tokens = [];
+                await req.user.save();
+                res.send();
+            } catch (e) {
+                res.status(500).send()
+            }
+        })
+    )
+}
 
-//delete my profile//
-router.delete('/users/me', auth, async (req, res) => {
-    try {
-        await req.user.remove();
-        res.send(req.user);
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+function defineGetAllUsersEndpoint() {
+    return (
+        router.get('/users', async (req, res) => {
+            try {
+                const users = await User.find({});
+                res.send(users);
+            } catch (e) {
+                res.status(500).send();
+            }
+        })
+    )
+}
+
+function defineGetUserProfileEndpoint() {
+    return (
+        router.get('/users/me', auth, async (req, res) => {
+            res.send(req.user);
+        })
+    )
+}
+
+function defineGetUserByPathEndpoint() {
+    return (
+        router.get('/users/:path', async (req, res) => {
+            const path = req.params.path;
+            try {
+                const user = await User.find({ path });
+                // const user = await User.findById(_id);
+                if (!user) {
+                    return res.status(404).send();
+                }
+                res.send(user);
+            } catch (e) {
+                res.status(500).send();
+            }
+        
+        })
+    )
+}
+
+function defineAddToMyFollowingEndpoint() {
+    return (
+        router.patch('/users/me/followUser', auth, async (req, res) => {
+            const path = req.body.path;
+            try {
+                // const userToFollow = await User.find({ path });
+                const userToFollow = await User.findOneAndUpdate({ path }, 
+                    {$addToSet: {myFollowers: {userId: req.user._id}}});
+                if (!userToFollow) {
+                    return res.status(404).send();
+                }
+                await req.user.updateOne(
+                    { $addToSet: { usersIFollow: { userId: userToFollow._id } } }
+                );
+                res.send(req.user);
+        
+            } catch (e) {
+                res.status(400).send(e);
+            }
+        })
+    )
+}
+
+function defineRemoveUserFromMyFollowingEndpoint() {
+    return (
+        router.patch('/users/me/unfollowUser', auth, async (req, res) => {
+            const path = req.body.path;
+            try {
+                const userToUnfollow = await User.findOneAndUpdate({ path }, 
+                    { $pull: { myFollowers: { userId: req.user._id } } });
+                if (!userToUnfollow) {
+                    return res.status(404).send();
+                }
+                await req.user.updateOne(
+                    { $pull: { usersIFollow: { userId: userToUnfollow._id } } }
+                );
+                res.send(req.user);
+        
+            } catch (e) {
+                res.status(400).send(e);
+            }
+        })
+    )
+}
+
+
+function defineUpdateMyProfileEndpoint() {
+    return (
+        router.patch('/users/me', auth, async (req, res) => {
+            const updates = Object.keys(req.body);
+            const allowedUpdates = ['firstName', 'lastName', 'password', 'email', 'age'];
+            const isValidOperation = updates.every((update) => {
+                return allowedUpdates.includes(update);
+            })
+        
+            if (!isValidOperation) {
+                return res.status(400).send({ error: "Invalid Updates" });
+            }
+        
+            try {
+                // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+                updates.forEach((update) => {
+                    req.user[update] = req.body[update];
+                })
+                await req.user.save();
+                res.send(req.user);
+            } catch (e) {
+                res.status(400).send(e);
+            }
+        })
+    )
+}
+
+function defineDeleteMyProfileEndpoint() {
+    return (
+        router.delete('/users/me', auth, async (req, res) => {
+            try {
+                await req.user.remove();
+                res.send(req.user);
+            } catch (e) {
+                res.status(500).send()
+            }
+        })
+    )
+}
 
 
 module.exports = router;
